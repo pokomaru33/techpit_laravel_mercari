375f886cabe3d006bc2181b2490ac5bb2b53c0e3
hrmt2 <horimoto@millamilla.jp>
2021-12-16 12:58:54 +0900

    add: vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,20 @@
 require('./bootstrap');
 
+window.Vue = require('vue').default;
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faAddressCard, faClock } from '@fortawesome/free-regular-svg-icons'
 import { faSearch, faStoreAlt, faShoppingBag, faSignOutAlt, faYenSign, faCamera } from '@fortawesome/free-solid-svg-icons'
 library.add(faSearch, faAddressCard, faStoreAlt, faShoppingBag, faSignOutAlt, faYenSign, faClock, faCamera);
 dom.watch();
+
+
+
+const app = new Vue({
+    el: '#app',
+});
 
 
 
